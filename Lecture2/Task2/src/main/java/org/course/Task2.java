48b8e1699a6d9b61915d7437a9a9c60a4fa97dc8
Dmitry Yablokov <yablokovdy@gmail.com>
2022-06-03 14:54:46 +0300

    replaced some vars

--- Lecture2/Task2/src/main/java/org/course/Task2.java
+++ Lecture2/Task2/src/main/java/org/course/Task2.java
@@ -6,6 +6,10 @@ public class Task2 {
     }
 
     public static void main(String[] args) {
+        int maxChar = 0;
+        String maxName = "";
+        double meanValue;
+        String result;
         String[] teachers = new String[] {"Белецкий Денис", "Брук Марк", "Ёлкин Александр", "Лозицкий Дмитрий", "Южаков Станислав"};
         String[] students = new String[] {"Яблоков Дмитрий", "Надолинский Артем", "Салатенков Илья", "Мамутов Сергей",
                 "Моисеев Артём", "Гагаева Мария", "Малышева Карина", "Федоренко Яна", "Положенцева Анна", "Шипулина Юлия", "Никифоров Максим"};
@@ -24,18 +28,14 @@ public class Task2 {
             }
             System.out.println(".");
         }
-        double countOfTeachers = teachers.length;
-        double countOfStudents = students.length;
-        double meanValue = countOfStudents / countOfTeachers;
-        String result = String.format("%.2f", meanValue);
-        int maxChar = 0;
-        String maxName = "";
-        for (int i = 0; i < students.length; i++) {
-            if (students[i].length() > maxChar) {
-                maxChar = students[i].length();
-                maxName = students[i];
+        meanValue = (double)students.length / (double)teachers.length;
+        result = String.format("%.2f", meanValue);
+        for (String student : students) {
+            if (student.length() > maxChar) {
+                maxChar = student.length();
+                maxName = student;
             }
         }
-        System.out.println("В среднем на каждого ментора приходится по " + meanValue + " студента Во всей группе самое длинное имя у '" + maxName + "'");
+        System.out.println("В среднем на каждого ментора приходится по " + result + " студента Во всей группе самое длинное имя у '" + maxName + "'");
     }
 }
