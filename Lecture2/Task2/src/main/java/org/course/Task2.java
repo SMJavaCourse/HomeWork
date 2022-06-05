99907c871aebe7f46b3c6f761d7690f6e00ab1d9
fundorick <fundorick@gmail.com>
2022-06-05 04:42:23 +0300

    ДЗ. Лекция 2. Основаное задание.

--- Lecture2/Task2/src/main/java/org/course/Task2.java
+++ Lecture2/Task2/src/main/java/org/course/Task2.java
@@ -6,5 +6,37 @@ public class Task2 {
         String[] teachers = new String[] {"Белецкий Денис", "Брук Марк", "Ёлкин Александр", "Лозицкий Дмитрий", "Южаков Станислав"};
         String[] students = new String[] {"Яблоков Дмитрий", "Надолинский Артем", "Салатенков Илья", "Мамутов Сергей",
                 "Моисеев Артём", "Гагаева Мария", "Малышева Карина", "Федоренко Яна", "Положенцева Анна", "Шипулина Юлия", "Никифоров Максим"};
+        appointer(teachers, students);
+        averageStudents(teachers, students);
+        longestName(students);
+    }
+
+    private static void appointer(String[] teachers, String[] students) {
+        for (int i = 0, mentorNumber = 1; i < teachers.length; i++, mentorNumber++) {
+            System.out.println("Ментор " + mentorNumber + ". \n" + teachers[i]);
+            System.out.println("Студенты:");
+            for (int j = i; j < students.length; j = j + teachers.length) {
+                System.out.print(students[j] + ", ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void averageStudents(String[] teachers, String[] students) {
+        float averageStudents = (float) students.length / (float) teachers.length;
+        String roundedResult = String.format("%.2f", averageStudents);
+        System.out.println("\nВ среднем на каждого ментора приходится по " + roundedResult + " студента.");
+    }
+
+    private static void longestName(String[] students) {
+        int maxLength = 0;
+        String longestName = "";
+        for (String student : students) {
+            if (student.length() > maxLength) {
+                maxLength = student.length();
+                longestName = student;
+            }
+        }
+        System.out.println("\nВо всей группе самое длинное имя у '" + longestName + "'");
     }
 }
