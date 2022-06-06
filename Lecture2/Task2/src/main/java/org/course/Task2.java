646aa0a8bcee30e54b209c37a1fe68bf162743ed
denis.chernov <denis.chernov@soft-machine.ru>
2022-06-07 00:52:56 +0300

    версия 1

--- Lecture2/Task2/src/main/java/org/course/Task2.java
+++ Lecture2/Task2/src/main/java/org/course/Task2.java
@@ -1,10 +1,30 @@
 package org.course;
 
+import static java.lang.System.*;
+
 public class Task2 {
 
     public static void main(String[] args) {
+        String stud_list = "";
+        String delimiter = ", ";
         String[] teachers = new String[] {"Белецкий Денис", "Брук Марк", "Ёлкин Александр", "Лозицкий Дмитрий", "Южаков Станислав"};
         String[] students = new String[] {"Яблоков Дмитрий", "Надолинский Артем", "Салатенков Илья", "Мамутов Сергей",
                 "Моисеев Артём", "Гагаева Мария", "Малышева Карина", "Федоренко Яна", "Положенцева Анна", "Шипулина Юлия", "Никифоров Максим"};
-    }
+        String[][] complex = new String[teachers.length][];
+        for (int i = 0; i < teachers.length; i++) {
+            out.println("Mentor 1: " + teachers[i]);
+            out.print("Students: ");
+            for (int j = i; j < students.length; j=j+teachers.length) {
+                if (j==i){
+                    stud_list = students[j];
+                }
+                else {
+                    stud_list = String.join(delimiter, stud_list, students[j]);
+                }
+            }
+            out.println(stud_list);
+            stud_list = "";
+        }
+        out.println("В среднем на каждого ментора приходится по "+ (double)students.length/teachers.length +" студента");
+        }
 }
