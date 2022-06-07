248661f298ca44c54865b4ccb877564fae08983f
artem.nadolinskij <artem.nadolinskij@soft-machine.ru>
2022-06-07 14:51:52 +0300

    добавил округление до 2 знаков

--- Lecture2/Task2/src/main/java/org/course/Task2.java
+++ Lecture2/Task2/src/main/java/org/course/Task2.java
@@ -2,30 +2,30 @@ package org.course;
 
 public class Task2 {
 
-    public static void main(String[] args) {
+    public static void main(String[] students) {
         String[] teachers = new String[] {"Белецкий Денис", "Брук Марк", "Ёлкин Александр", "Лозицкий Дмитрий", "Южаков Станислав"};
 
         String longName = "";
-        float count = (float) args.length / (float) teachers.length;
+        float count = (float) students.length / (float) teachers.length;
 
         System.out.println();
 
         for (int i = 0; i < teachers.length; i++) {
             System.out.println("Ментор "+(i+1)+":" + "\n" + teachers[i] + "\n" + "Студенты:");
-            for (int j = i; j < args.length; j+= teachers.length) {
-                System.out.print(args[j]);
-                if (j + teachers.length <= args.length-1) {
+            for (int j = i; j < students.length; j+= teachers.length) {
+                System.out.print(students[j]);
+                if (j + teachers.length <= students.length-1) {
                     System.out.print(", ");
                 }
                 else {
                     System.out.println();
                 }
-                if (longName.length() < args[j].length()) {
-                    longName = args[j];
+                if (longName.length() < students[j].length()) {
+                    longName = students[j];
                 }
             }
         }
-        System.out.println("\n" + "В среднем на каждого ментора приходится по " + count + " студента" +
-                "\n" + "Во всей группе самое длинное имя у '" + longName + "'");
+        System.out.println("\n" + "В среднем на каждого ментора приходится по " + String.format("%.2f",count) +
+                " студента" + "\n" + "Во всей группе самое длинное имя у '" + longName + "'");
     }
 }
