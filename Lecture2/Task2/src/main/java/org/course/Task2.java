d7b6d84718a91ea1241e74ab411167122c22f1e8
denis.chernov <denis.chernov@soft-machine.ru>
2022-06-07 13:36:27 +0300

    версия 2 с вычислением максимально длинного имени

--- Lecture2/Task2/src/main/java/org/course/Task2.java
+++ Lecture2/Task2/src/main/java/org/course/Task2.java
@@ -1,5 +1,7 @@
 package org.course;
 
+import java.util.*;
+
 import static java.lang.System.*;
 
 public class Task2 {
@@ -7,6 +9,8 @@ public class Task2 {
     public static void main(String[] args) {
         String stud_list = "";
         String delimiter = ", ";
+        int max = 0;
+        String max_name = "";
         String[] teachers = new String[] {"Белецкий Денис", "Брук Марк", "Ёлкин Александр", "Лозицкий Дмитрий", "Южаков Станислав"};
         String[] students = new String[] {"Яблоков Дмитрий", "Надолинский Артем", "Салатенков Илья", "Мамутов Сергей",
                 "Моисеев Артём", "Гагаева Мария", "Малышева Карина", "Федоренко Яна", "Положенцева Анна", "Шипулина Юлия", "Никифоров Максим"};
@@ -15,16 +19,16 @@ public class Task2 {
             out.println("Mentor 1: " + teachers[i]);
             out.print("Students: ");
             for (int j = i; j < students.length; j=j+teachers.length) {
-                if (j==i){
-                    stud_list = students[j];
-                }
-                else {
-                    stud_list = String.join(delimiter, stud_list, students[j]);
+                stud_list = (j==i) ?  stud_list = students[j] : String.join(delimiter, stud_list, students[j]);
+                if (max < students[j].length()){
+                    max=students[j].length();
+                    max_name = students[j];
                 }
             }
             out.println(stud_list);
             stud_list = "";
         }
         out.println("В среднем на каждого ментора приходится по "+ (double)students.length/teachers.length +" студента");
+        out.println("Во всей группе самое длинное имя у "+ max_name +". Длина имени равна "+ max);
         }
 }
