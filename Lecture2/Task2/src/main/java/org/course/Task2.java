67d3c33c1de318767d642d87c8a7c1e9ca9ed789
artem.nadolinskij <artem.nadolinskij@soft-machine.ru>
2022-06-07 16:24:40 +0300

    рефакторинг

--- Lecture2/Task2/src/main/java/org/course/Task2.java
+++ Lecture2/Task2/src/main/java/org/course/Task2.java
@@ -3,29 +3,30 @@ package org.course;
 public class Task2 {
 
     public static void main(String[] students) {
-        String[] teachers = new String[] {"Белецкий Денис", "Брук Марк", "Ёлкин Александр", "Лозицкий Дмитрий", "Южаков Станислав"};
+        String[] teachers = new String[]{"Белецкий Денис", "Брук Марк", "Ёлкин Александр", "Лозицкий Дмитрий", "Южаков Станислав"};
 
         String longName = "";
         float count = (float) students.length / (float) teachers.length;
-
         System.out.println();
 
         for (int i = 0; i < teachers.length; i++) {
-            System.out.println("Ментор "+(i+1)+":" + "\n" + teachers[i] + "\n" + "Студенты:");
-            for (int j = i; j < students.length; j+= teachers.length) {
-                System.out.print(students[j]);
-                if (j + teachers.length <= students.length-1) {
-                    System.out.print(", ");
-                }
-                else {
-                    System.out.println();
-                }
-                if (longName.length() < students[j].length()) {
-                    longName = students[j];
-                }
+            System.out.print("Ментор " + (i + 1) + ":" + "\n" + teachers[i] + "\n" + "Студенты:" + "" + "\n" + students[i]);
+            if (longName.length() < students[i].length()) {
+                longName = students[i];
             }
+            int nextStudent = i;
+            do {
+                System.out.print(", ");
+                nextStudent += teachers.length;
+                System.out.print(students[nextStudent]);
+                if (longName.length() < students[nextStudent].length()) {
+                    longName = students[nextStudent];
+                }
+            } while (nextStudent + teachers.length < students.length);
+            System.out.println("");
         }
+
         System.out.println("\n" + "В среднем на каждого ментора приходится по " + String.format("%.2f",count) +
-                " студента" + "\n" + "Во всей группе самое длинное имя у '" + longName + "'");
+                 " студента" + "\n" + "Во всей группе самое длинное имя у '" + longName + "'");
     }
 }
