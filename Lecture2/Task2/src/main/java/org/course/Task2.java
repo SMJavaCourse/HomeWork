4592827930dd6af2cc3e98cebc5d5a0f8182cb7f
artem.nadolinskij <artem.nadolinskij@soft-machine.ru>
2022-06-07 19:16:11 +0300

    вернул как было

--- Lecture2/Task2/src/main/java/org/course/Task2.java
+++ Lecture2/Task2/src/main/java/org/course/Task2.java
@@ -3,34 +3,29 @@ package org.course;
 public class Task2 {
 
     public static void main(String[] students) {
-        String[] teachers = new String[]{"Белецкий Денис", "Брук Марк", "Ёлкин Александр", "Лозицкий Дмитрий", "Южаков Станислав"};
-        String longName = "";
+        String[] teachers = new String[] {"Белецкий Денис", "Брук Марк", "Ёлкин Александр", "Лозицкий Дмитрий", "Южаков Станислав"};
 
+        String longName = "";
         float count = (float) students.length / (float) teachers.length;
 
         System.out.println();
 
         for (int i = 0; i < teachers.length; i++) {
-            System.out.print("Ментор " + (i + 1) + ":" + "\n" + teachers[i] + "\n" + "Студенты:" + "" + "\n");
-            int nextStudent = i;
-            try {
-                System.out.print(students[i]);
-            do {
-                nextStudent += teachers.length;
-                if (longName.length() < students[i].length()) {
-                    longName = students[i];
+            System.out.println("Ментор "+(i+1)+":" + "\n" + teachers[i] + "\n" + "Студенты:");
+            for (int j = i; j < students.length; j+= teachers.length) {
+                System.out.print(students[j]);
+                if (j + teachers.length <= students.length-1) {
+                    System.out.print(", ");
                 }
-                else if (longName.length() < students[nextStudent].length()) {
-                    longName = students[nextStudent];
+                else {
+                    System.out.println();
                 }
-                    System.out.print(", " + students[nextStudent]);
-                } while (nextStudent + teachers.length < students.length);
-            } catch (ArrayIndexOutOfBoundsException a){
-                //хз, что сюда записать, ловить ловить исключение не планируется
+                if (longName.length() < students[j].length()) {
+                    longName = students[j];
+                }
             }
-            System.out.println();
         }
         System.out.println("\n" + "В среднем на каждого ментора приходится по " + String.format("%.2f",count) +
-                 " студента" + "\n" + "Во всей группе самое длинное имя у '" + longName + "'");
+                " студента" + "\n" + "Во всей группе самое длинное имя у '" + longName + "'");
     }
 }
