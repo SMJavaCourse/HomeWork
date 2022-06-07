c763f1d93402ee8516f97798ef786de26884c048
artem.nadolinskij <artem.nadolinskij@soft-machine.ru>
2022-06-07 13:02:00 +0300

    использование program arguments

--- Lecture2/Task2/src/main/java/org/course/Task2.java
+++ Lecture2/Task2/src/main/java/org/course/Task2.java
@@ -4,26 +4,24 @@ public class Task2 {
 
     public static void main(String[] args) {
         String[] teachers = new String[] {"Белецкий Денис", "Брук Марк", "Ёлкин Александр", "Лозицкий Дмитрий", "Южаков Станислав"};
-        String[] students = new String[] {"Яблоков Дмитрий", "Надолинский Артем", "Салатенков Илья", "Мамутов Сергей",
-                "Моисеев Артём", "Гагаева Мария", "Малышева Карина", "Федоренко Яна", "Положенцева Анна", "Шипулина Юлия", "Никифоров Максим"};
 
         String longName = "";
-        float count = (float) students.length / (float) teachers.length;
+        float count = (float) args.length / (float) teachers.length;
 
         System.out.println();
 
         for (int i = 0; i < teachers.length; i++) {
             System.out.println("Ментор "+(i+1)+":" + "\n" + teachers[i] + "\n" + "Студенты:");
-            for (int j = i; j < students.length; j+= teachers.length) {
-                System.out.print(students[j]);
-                if (j + teachers.length <= students.length-1) {
+            for (int j = i; j < args.length; j+= teachers.length) {
+                System.out.print(args[j]);
+                if (j + teachers.length <= args.length-1) {
                     System.out.print(", ");
                 }
                 else {
                     System.out.println();
                 }
-                if (longName.length() < students[j].length()) {
-                    longName = students[j];
+                if (longName.length() < args[j].length()) {
+                    longName = args[j];
                 }
             }
         }
