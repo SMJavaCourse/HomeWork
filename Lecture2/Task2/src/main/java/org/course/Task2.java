f0e3fbe900501acc3ec7b721d03981f0203e5429
Maksim Nikiforov <maksim.nikiforov@soft-machine.ru>
2022-06-08 19:00:27 +0300

    Поправил багу с циклом с самым длинным в группе именем

--- Lecture2/Task2/src/main/java/org/course/Task2.java
+++ Lecture2/Task2/src/main/java/org/course/Task2.java
@@ -8,28 +8,28 @@ public class Task2 {
                 "Моисеев Артём", "Гагаева Мария", "Малышева Карина", "Федоренко Яна", "Положенцева Анна", "Шипулина Юлия", "Никифоров Максим"};
         double teachersSize = teachers.length;
         double overline = students.length / teachersSize;
-        System.out.printf("Среднее количество студентов: %.2f %n", overline);
-        String maxLength = "";
-        String maxStudentLenght= "";
+        System.out.printf("Среднее количество студентов: %.2f", overline);
+        String maxGroupNameLength = "";
+        String maxStudentsLength = "";
         for (int i = 0; i < teachers.length; i++) {
             System.out.println("\nГруппа № " + i + ":");
             System.out.println("Ментор: " + teachers[i]);
+            maxGroupNameLength = teachers[i];
             for (int count = i; count < students.length; count = (count + teachers.length)) {
-                maxLength = teachers[i];
-                if (students[count].length()>maxStudentLenght.length()){
-                    maxStudentLenght= students[count];
+                if (students[count].length() > maxStudentsLength.length()) {
+                    maxStudentsLength = students[count];
                 }
-                if (students[count].length() > maxLength.length()) {
-                    maxLength = students[count];
+                if (students[count].length() > maxGroupNameLength.length()) {
+                    maxGroupNameLength = students[count];
                 }
                 if (count != i) {
                     System.out.print(", ");
                 }
                 System.out.print(students[count]);
             }
-            System.out.println("\nСамое длинное значение в группе: " + maxLength);
+            System.out.println("\nСамое длинное значение в группе: " + maxGroupNameLength);
         }
-        System.out.println("\nВаще самое длинное: " + maxStudentLenght +" =)");
+        System.out.println("\nВаще самое длинное: " + maxStudentsLength + " =)");
     }
 }
 
