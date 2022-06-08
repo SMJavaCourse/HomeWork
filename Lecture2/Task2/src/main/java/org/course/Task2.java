5dd36b7f8389f60de649a0159d2de1a8c9e595b2
ArtemMoiseyev <>
2022-06-08 19:06:53 +0300

    Исправленный вариант домашнего задания 2

--- Lecture2/Task2/src/main/java/org/course/Task2.java
+++ Lecture2/Task2/src/main/java/org/course/Task2.java
@@ -6,49 +6,43 @@ public class Task2 {
         String[] teachers = new String[] {"Белецкий Денис", "Брук Марк", "Ёлкин Александр", "Лозицкий Дмитрий", "Южаков Станислав"};
         String[] students = new String[] {"Яблоков Дмитрий", "Надолинский Артем", "Салатенков Илья", "Мамутов Сергей",
                 "Моисеев Артём", "Гагаева Мария", "Малышева Карина", "Федоренко Яна", "Положенцева Анна", "Шипулина Юлия", "Никифоров Максим"};
-        int nTeach = 0;
-        int nStud = 0;
-        int max = 0;
-        int q = 0;
-        nTeach = teachers.length;
-        nStud = students.length;
-        String [][] ds  = new String[nTeach] [nStud];
-        int[] ct = new int[nTeach];
-        int[] mV = new int[nStud];
-        for (int i = 0; i < nTeach; i = i + 1) {
+        int numberOfTeachers = teachers.length;
+        int numberOfStudents = students.length;
+        int maximumLengthOfName = 0;
+        int maximumLengthOfNameIndex = 0;
+
+        int[] lengthOfName = new int[numberOfStudents];
+        for (int i = 0; i < numberOfTeachers; i = i + 1) {
             int n = i + 1;
             System.out.println("Ментор" + " " + n + ":");
             System.out.println(teachers[i]);
             System.out.println(" ");
             System.out.println("Студенты:");
-            for (int j = i; j < nStud; j = j + nTeach) {
+            for (int j = i; j < numberOfStudents; j = j + numberOfTeachers) {
                 System.out.println(students[j] + " ");
-                ct[i] = ct[i]+1;
+                lengthOfName[j] = students[j].length();
             }
             System.out.println(" ");
 
         }
 
-        for (int t = 0; t < nStud; t++) {
-            mV[t] = students[t].length();
-            if (mV[t] > max) {
-                q = t;
+        for (int i = 0; i < numberOfStudents; i++) {
+            if (lengthOfName[i] > maximumLengthOfName) {
+                maximumLengthOfName = lengthOfName[i];
+                maximumLengthOfNameIndex = i;
             }
 
         }
 
-        double av = 0;
-            double s = 0;
-            for (int x = 0; x < nTeach; x++) {
-                s += ct[x];
-            }
-            av = s / nTeach;
+        float numOfStud = numberOfStudents;
+        float numOfTea = numberOfTeachers;
+
+        float averageQuantityOfStudents = numOfStud / numOfTea;
 
 
+        System.out.println("В среднем на каждого ментора приходится по" + " " + averageQuantityOfStudents + " " + "студентов");
 
-        System.out.println("В среднем на каждого ментора приходится по" + " " + av + " " + "студентов");
-
-        System.out.println("Во всей группе самое длинное имя у студента, которого зовут" + " " + students[q]);
+        System.out.println("Во всей группе самое длинное имя у студента, которого зовут" + " " + students[maximumLengthOfNameIndex]);
 
 
 
