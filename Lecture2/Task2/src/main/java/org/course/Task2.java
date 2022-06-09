de4c3c17618f819b1527c7865a240ecfd6d17da0
Maksim Nikiforov <maksim.nikiforov@soft-machine.ru>
2022-06-09 10:55:18 +0300

    Добавил чтение из аргумента

--- Lecture2/Task2/src/main/java/org/course/Task2.java
+++ Lecture2/Task2/src/main/java/org/course/Task2.java
@@ -2,8 +2,7 @@ package org.course;
 
 public class Task2 {
 
-    public static void main(String[] args) {
-        String[] teachers = new String[]{"Белецкий Денис", "Брук Марк", "Ёлкин Александр", "Лозицкий Дмитрий", "Южаков Станислав"};
+    public static void main(String[] teachers) {
         String[] students = new String[]{"Яблоков Дмитрий", "Надолинский Артем", "Салатенков Илья", "Мамутов Сергей",
                 "Моисеев Артём", "Гагаева Мария", "Малышева Карина", "Федоренко Яна", "Положенцева Анна", "Шипулина Юлия", "Никифоров Максим"};
         double teachersSize = teachers.length;
