496a7ed9c8cf5cda55140fd97140a5fab089e677
Ilya Salatenkov <ilya.salatenkov@soft-machine.ru>
2022-06-09 12:05:38 +0300

    task2 поправил названия переменных, формат код

--- Lecture2/Task2/src/main/java/org/course/Task2.java
+++ Lecture2/Task2/src/main/java/org/course/Task2.java
@@ -7,36 +7,36 @@ public class Task2 {
         String[] students = new String[]{"Яблоков Дмитрий", "Надолинский Артем", "Салатенков Илья", "Мамутов Сергей",
                 "Моисеев Артём", "Гагаева Мария", "Малышева Карина", "Федоренко Яна", "Положенцева Анна", "Шипулина Юлия", "Никифоров Максим"};
 
-        for (int i=0; i < teachers.length; ++i) {
-            System.out.println("Ментор " + (i+1) + ":" + "\n" + teachers[i] + "\n" + "Студенты:");
+        for (int i = 0; i < teachers.length; ++i) {
+            System.out.println("Ментор " + (i + 1) + ":" + "\n" + teachers[i] + "\n" + "Студенты:");
             String longestName = teachers[i];
 
-            for (int f=i; f<students.length; f=f+teachers.length) {
+            for (int f = i; f < students.length; f = f + teachers.length) {
                 System.out.println(students[f]);
-                if (students[f].length()>students[i].length()) {
+                if (students[f].length() > students[i].length()) {
                     longestName = students[f];
-                } else if (students[i].length()> teachers[i].length()) {
+                } else if (students[i].length() > teachers[i].length()) {
                     longestName = students[i];
                 }
             }
             System.out.println("-----------Дольше всех в группе своё имя пишет " + longestName + "-----------");
             System.out.println("\n");
         }
-        float value = students.length*10/ teachers.length;
-        value = value/10;
+        float value = (float) students.length * 100 / teachers.length;
+        value = value / 100;
         String result = String.format("%.2f", value);
         System.out.println("В среднем на каждого ментора приходится " + result + " студента");
-        String m = teachers[0];
-        for (int n=1; teachers.length>n; ++n){
-            if (teachers[n].length()>=m.length()){
-                m=teachers[n];
+        String allLongName = teachers[0];
+        for (int i = 1; teachers.length > i; ++i) {
+            if (teachers[i].length() >= allLongName.length()) {
+                allLongName = teachers[i];
             }
         }
-        for (int i=0; students.length>i; ++i){
-            if (students[i].length()>=m.length()){
-                m=students[i];
+        for (int i = 0; students.length > i; ++i) {
+            if (students[i].length() >= allLongName.length()) {
+                allLongName = students[i];
             }
         }
-        System.out.println("А совсем-совсем дольше всех пишет имя " + m);
+        System.out.println("А совсем-совсем дольше всех пишет имя " + allLongName);
     }
 }
