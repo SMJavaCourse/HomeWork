2708d49e87b8494c90db2c0c219834b159642bd0
anna polozhentseva <anna1-beta-gos-2@yandex.ru>
2022-06-18 17:14:27 +0300

    Задание второй лекции. Распределение студентов между менторами

--- Lecture2/Task2/src/main/java/org/course/Task2.java
+++ Lecture2/Task2/src/main/java/org/course/Task2.java
@@ -6,28 +6,29 @@ public class Task2 {
         String[] teachers = new String[] {"Белецкий Денис", "Брук Марк", "Ёлкин Александр", "Лозицкий Дмитрий", "Южаков Станислав"};
         String[] students = new String[] {"Яблоков Дмитрий", "Надолинский Артем", "Салатенков Илья", "Мамутов Сергей",
                 "Моисеев Артём", "Гагаева Мария", "Малышева Карина", "Федоренко Яна", "Положенцева Анна", "Шипулина Юлия", "Никифоров Максим"};
-
+        String max = students[0];
         for (int j=0; j< teachers.length; j++){
             System.out.println("Ментор " +  (j+1) + ": ");
             System.out.println(teachers[j]);
             System.out.println("Студенты: ");
             for (int i = j; i< students.length; i=i + teachers.length){
+
+                if (max.length()<students[i].length()) {
+                    max=students[i];
+                }
                 System.out.print(students[i] +", ");
+
             }
             System.out.println();
+
         }
 
-        float a = (float) students.length/ (float) teachers.length;
+        float a = students.length/ (float)  teachers.length;
         System.out.println();
         System.out.println("В среднем на каждого ментора приходится по: " + a + " студента");
-
-        String max = students[0];
-        for (int i = 1; i < students.length; i++) {
-            if (max.length()<students[i].length()) {
-                max=students[i];
-            }}
-
         System.out.println("Во всей группе самое длинное имя у: " + max);
+
+
 
 
 
