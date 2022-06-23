46505cf576024356b79e1604a859d42585ca9dbf
artem.nadolinskij <artem.nadolinskij@soft-machine.ru>
2022-06-23 21:18:39 +0300

    домашка 3

--- Lecture3/Task3/src/main/java/org/course/Task3.java
+++ Lecture3/Task3/src/main/java/org/course/Task3.java
@@ -10,20 +10,21 @@ public class Task3 {
     public static String oneRoomApatrtment = "Однокомнатный номер";
     public static String twoRoomApatrtment = "Двухкомнатный номер";
     public static String threeRoomApatrtment = "Трехкомнатный номер";
-    public static String startTime = "09:00";
+    public static String startTime1 = "12:00";
+    public static String startTime2 = "09:00";
 
     public static void main(String[] args) {
 
         Apartment[] allRoomsInHotel1 = new Apartment[5];
-        Apartment room11 = new Apartment(oneRoomApatrtment, 1000, 1, 2, startTime, balcony,
+        Apartment room11 = new Apartment(oneRoomApatrtment, 1000, 1, 2, startTime1, balcony,
                 null, null, null, null);
-        Apartment room12 = new Apartment(twoRoomApatrtment, 3000, 2, 4, startTime, balcony,
+        Apartment room12 = new Apartment(twoRoomApatrtment, 3000, 2, 4, startTime1, balcony,
                 cleaning, internet, conditioner, null);
-        Apartment room13 = new Apartment(twoRoomApatrtment, 3000, 3, 4, startTime, balcony,
+        Apartment room13 = new Apartment(twoRoomApatrtment, 3000, 3, 4, startTime1, balcony,
                 cleaning, internet, conditioner, null);
-        Apartment room14 = new Apartment(threeRoomApatrtment, 2500, 4, 6, startTime,null,
+        Apartment room14 = new Apartment(threeRoomApatrtment, 2500, 4, 6, startTime1,null,
                 null, null,null, null);
-        Apartment room15 = new Apartment(threeRoomApatrtment, 3500, 5, 6, startTime, balcony,
+        Apartment room15 = new Apartment(threeRoomApatrtment, 3500, 5, 6, startTime1, balcony,
                 null, internet, null, null);
 
         allRoomsInHotel1[0] = room11;
@@ -33,11 +34,11 @@ public class Task3 {
         allRoomsInHotel1[4] = room15;
 
         Apartment[] allRoomsInHotel2 = new Apartment[3];
-        Apartment room21 = new Apartment(oneRoomApatrtment, 1000, 1, 2, startTime,null,
+        Apartment room21 = new Apartment(oneRoomApatrtment, 1000, 1, 2, startTime2,null,
                 null, null, null, meat);
-        Apartment room22 = new Apartment(twoRoomApatrtment, 2000, 2, 4, startTime, balcony,
+        Apartment room22 = new Apartment(twoRoomApatrtment, 2000, 2, 4, startTime2, balcony,
                 cleaning, null, null, null);
-        Apartment room23 = new Apartment(threeRoomApatrtment, 4000, 3, 6, startTime,null,
+        Apartment room23 = new Apartment(threeRoomApatrtment, 4000, 3, 6, startTime2,null,
                 cleaning, null, null, null);
 
         allRoomsInHotel2[0] = room21;
