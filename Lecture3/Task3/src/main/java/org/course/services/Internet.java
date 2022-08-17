b3e666e730f3d35195dfec98503f9a5c40ff5275
artem.nadolinskij <artem.nadolinskij@soft-machine.ru>
2022-08-17 12:53:20 +0300

    добавлены оба конструктора для класса Internet аннотацией lombok

--- Lecture3/Task3/src/main/java/org/course/services/Internet.java
+++ Lecture3/Task3/src/main/java/org/course/services/Internet.java
@@ -1,15 +1,14 @@
 package org.course.services;
 
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class Internet extends Services {
 
     int speed = 256;
-
-    public Internet(int speed) {
-        this.speed = speed;
-    }
-
-    public Internet() {
-    }
 
     public String getName(){
         return "интернет (скорость соединения " + speed + " Кб\\с)";
