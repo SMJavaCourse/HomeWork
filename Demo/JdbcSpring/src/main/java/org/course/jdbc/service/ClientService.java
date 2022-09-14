3942c6249a658f946688990ae47eb6305d9d2c52
rukbrook <rukbrook@gmail.com>
2022-09-15 00:33:40 +0300

    finish controller

--- Demo/JdbcSpring/src/main/java/org/course/jdbc/service/ClientService.java
+++ Demo/JdbcSpring/src/main/java/org/course/jdbc/service/ClientService.java
@@ -3,29 +3,25 @@ package org.course.jdbc.service;
 import lombok.RequiredArgsConstructor;
 import org.course.jdbc.converter.Converter;
 import org.course.jdbc.dto.ClientInfo;
-import org.course.jdbc.entity.CarEntity;
-import org.course.jdbc.repository.CarRepository;
 import org.course.jdbc.repository.ClientRepository;
-import org.course.jdbc.repository.RepairRequestHistoryRepository;
 import org.course.jdbc.repository.RepairRequestRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.UUID;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class ClientService {
-    private final CarRepository carRepository;
     private final ClientRepository clientRepository;
     private final RepairRequestRepository repairRequestRepository;
-    private final RepairRequestHistoryRepository repairRequestHistoryRepository;
     private final Converter converter;
 
     public ClientInfo getClientInfo(String id) {
-        var client = clientRepository.findById("14fbd742-ab98-4730-a58d-9f0ac72126bf").get();
+        var optionalClient = clientRepository.findById(id);
+        if (optionalClient.isEmpty()) {
+            return null;
+        }
+        var client = optionalClient.get();
         var repairRequests = repairRequestRepository.findAllByClientId(client.getId());
 
         return ClientInfo.builder()
