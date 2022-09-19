db1746c24edc614e38e1d32787e95ad2d503c2f4
artem.nadolinskij <artem.nadolinskij@soft-machine.ru>
2022-09-19 12:53:28 +0300

    test commit from new account

--- Lecture11HomeWork/src/test/java/org/course/hotels/given/Given.java
+++ Lecture11HomeWork/src/test/java/org/course/hotels/given/Given.java
@@ -63,7 +63,7 @@ public class Given {
 
     public Hotel randomHotel(EasyRandom easyRandom) {
         ArrayList time = new ArrayList<>(Arrays.asList("07:00", "08:00", "09:00", "10:00", "11:00", "12:00",
-                "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00"));
+                "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"));
         String startTime = time.get(random.nextInt(time.size())).toString();
 
         ArrayList<Apartment> apartments = new ArrayList<>();
